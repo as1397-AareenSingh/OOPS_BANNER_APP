004fc269c3db58e3c9a19fc4f76295046b5a5707
as1397-AareenSingh <as1397@srmist.edu.in>
2026-03-03 21:42:43 +0530

    IMPLEMENTED CALCULATOR

--- Week1.java
+++ Week1.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class Week1 {
     public static void main(String[] args) {
         
-        double kilometers = 10.8;
-        double miles = kilometers * 1.6;   // Given: 1 km = 1.6 miles
+        Scanner sc = new Scanner(System.in);
         
-        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+        System.out.print("Enter first number: ");
+        double number1 = sc.nextDouble();
+        
+        System.out.print("Enter second number: ");
+        double number2 = sc.nextDouble();
+        
+        double addition = number1 + number2;
+        double subtraction = number1 - number2;
+        double multiplication = number1 * number2;
+        double division = number1 / number2;
+        
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers "
+                + number1 + " and " + number2 + " is "
+                + addition + ", "
+                + subtraction + ", "
+                + multiplication + ", and "
+                + division);
+        
+        sc.close();
     }
 }
