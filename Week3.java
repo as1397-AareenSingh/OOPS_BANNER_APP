6eb29b58c7cd2b4edd4581ea5da3023bcb317391
as1397-AareenSingh <as1397@srmist.edu.in>
2026-03-04 01:29:17 +0530

    IMPLEMENTED Largest

--- Week3.java
+++ Week3.java
@@ -14,9 +14,13 @@ public class Week3 {
         System.out.print("Enter third number: ");
         double number3 = sc.nextDouble();
 
-        boolean isSmallest = (number1 < number2) && (number1 < number3);
+        boolean isFirstLargest = (number1 > number2) && (number1 > number3);
+        boolean isSecondLargest = (number2 > number1) && (number2 > number3);
+        boolean isThirdLargest = (number3 > number1) && (number3 > number2);
 
-        System.out.println("Is the first number the smallest? " + isSmallest);
+        System.out.println("Is the first number the largest? " + isFirstLargest);
+        System.out.println("Is the second number the largest? " + isSecondLargest);
+        System.out.println("Is the third number the largest? " + isThirdLargest);
 
         sc.close();
     }
