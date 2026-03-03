7717109510851a9d5e431c0e6b2eab70b5edb6b2
as1397-AareenSingh <as1397@srmist.edu.in>
2026-03-04 01:49:44 +0530

    IMPLEMENTED Divisible

--- Week3.java
+++ Week3.java
@@ -5,49 +5,12 @@ public class Week3 {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter year: ");
-        int year = sc.nextInt();
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
 
-        // -------------------------------
-        // Part 1: Using Multiple If-Else
-        // -------------------------------
+        boolean isDivisible = (number % 5 == 0);
 
-        if (year < 1582) {
-            System.out.println("Leap year calculation works only for year >= 1582");
-        } else {
-
-            boolean isLeap = false;
-
-            if (year % 4 == 0) {
-                if (year % 100 == 0) {
-                    if (year % 400 == 0) {
-                        isLeap = true;
-                    } else {
-                        isLeap = false;
-                    }
-                } else {
-                    isLeap = true;
-                }
-            } else {
-                isLeap = false;
-            }
-
-            if (isLeap) {
-                System.out.println("Year is a Leap Year (Using multiple if-else)");
-            } else {
-                System.out.println("Year is not a Leap Year (Using multiple if-else)");
-            }
-
-            // ----------------------------------------
-            // Part 2: Using Single If with Logical Operators
-            // ----------------------------------------
-
-            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-                System.out.println("Year is a Leap Year (Using single if condition)");
-            } else {
-                System.out.println("Year is not a Leap Year (Using single if condition)");
-            }
-        }
+        System.out.println("Is the number " + number + " divisible by 5? " + isDivisible);
 
         sc.close();
     }
