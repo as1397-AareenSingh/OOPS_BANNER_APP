ee481807eda1ffe383e1f10bfddc2feb11151c03
as1397-AareenSingh <as1397@srmist.edu.in>
2026-03-04 01:42:03 +0530

    IMPLEMENTED LeapYear

--- Week3.java
+++ Week3.java
@@ -5,19 +5,49 @@ public class Week3 {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter salary: ");
-        double salary = sc.nextDouble();
+        System.out.print("Enter year: ");
+        int year = sc.nextInt();
 
-        System.out.print("Enter years of service: ");
-        int years = sc.nextInt();
+        // -------------------------------
+        // Part 1: Using Multiple If-Else
+        // -------------------------------
 
-        double bonus = 0;
+        if (year < 1582) {
+            System.out.println("Leap year calculation works only for year >= 1582");
+        } else {
 
-        if (years > 5) {
-            bonus = salary * 0.05;   // 5% bonus
+            boolean isLeap = false;
+
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        isLeap = true;
+                    } else {
+                        isLeap = false;
+                    }
+                } else {
+                    isLeap = true;
+                }
+            } else {
+                isLeap = false;
+            }
+
+            if (isLeap) {
+                System.out.println("Year is a Leap Year (Using multiple if-else)");
+            } else {
+                System.out.println("Year is not a Leap Year (Using multiple if-else)");
+            }
+
+            // ----------------------------------------
+            // Part 2: Using Single If with Logical Operators
+            // ----------------------------------------
+
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println("Year is a Leap Year (Using single if condition)");
+            } else {
+                System.out.println("Year is not a Leap Year (Using single if condition)");
+            }
         }
-
-        System.out.println("The bonus amount is " + bonus);
 
         sc.close();
     }
