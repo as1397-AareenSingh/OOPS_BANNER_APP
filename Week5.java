59e24180ed1239e9b55e31670eda286fb0672aa2
as1397-AareenSingh <as1397@srmist.edu.in>
2026-03-15 13:27:00 +0530

    IMPLEMENTED ELIGIBILITY

---
+++ Week5.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class oopsbannerapp
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int age[] = new int[10];
+
+        // Taking input for 10 students
+        for(int i = 0; i < age.length; i++)
+        {
+            System.out.print("Enter age of student " + (i + 1) + ": ");
+            age[i] = sc.nextInt();
+        }
+
+        // Checking voting eligibility
+        for(int i = 0; i < age.length; i++)
+        {
+            if(age[i] < 0)
+            {
+                System.out.println("Invalid age");
+            }
+            else if(age[i] >= 18)
+            {
+                System.out.println("The student with the age " + age[i] + " can vote.");
+            }
+            else
+            {
+                System.out.println("The student with the age " + age[i] + " cannot vote.");
+            }
+        }
+
+        sc.close();
+    }
+}
