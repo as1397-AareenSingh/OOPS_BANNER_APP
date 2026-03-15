c30a5c22e997c19a19faac3df7730418cfa94adc
as1397-AareenSingh <as1397@srmist.edu.in>
2026-03-15 13:45:31 +0530

    IMPLEMENTED  TALL

--- Week5.java
+++ Week5.java
@@ -6,63 +6,43 @@ public class oopsbannerapp
     {
         Scanner sc = new Scanner(System.in);
 
-        double salary[] = new double[10];
-        double years[] = new double[10];
-        double bonus[] = new double[10];
-        double newSalary[] = new double[10];
-
-        double totalBonus = 0;
-        double totalOldSalary = 0;
-        double totalNewSalary = 0;
+        String names[] = {"Amar", "Akbar", "Anthony"};
+        int age[] = new int[3];
+        double height[] = new double[3];
 
         // Taking input
-        for(int i = 0; i < 10; i++)
+        for(int i = 0; i < 3; i++)
         {
-            System.out.println("Enter salary of employee " + (i+1) + ": ");
-            salary[i] = sc.nextDouble();
+            System.out.print("Enter age of " + names[i] + ": ");
+            age[i] = sc.nextInt();
 
-            System.out.println("Enter years of service of employee " + (i+1) + ": ");
-            years[i] = sc.nextDouble();
+            System.out.print("Enter height of " + names[i] + ": ");
+            height[i] = sc.nextDouble();
+        }
 
-            if(salary[i] <= 0 || years[i] < 0)
+        // Finding youngest
+        int youngestIndex = 0;
+        for(int i = 1; i < 3; i++)
+        {
+            if(age[i] < age[youngestIndex])
             {
-                System.out.println("Invalid input. Please enter again.");
-                i--; // repeat input for the same employee
+                youngestIndex = i;
             }
         }
 
-        // Calculating bonus and new salary
-        for(int i = 0; i < 10; i++)
+        // Finding tallest
+        int tallestIndex = 0;
+        for(int i = 1; i < 3; i++)
         {
-            if(years[i] > 5)
+            if(height[i] > height[tallestIndex])
             {
-                bonus[i] = salary[i] * 0.05;
+                tallestIndex = i;
             }
-            else
-            {
-                bonus[i] = salary[i] * 0.02;
-            }
-
-            newSalary[i] = salary[i] + bonus[i];
-
-            totalBonus += bonus[i];
-            totalOldSalary += salary[i];
-            totalNewSalary += newSalary[i];
         }
 
         // Display results
-        for(int i = 0; i < 10; i++)
-        {
-            System.out.println("Employee " + (i+1));
-            System.out.println("Old Salary: " + salary[i]);
-            System.out.println("Bonus: " + bonus[i]);
-            System.out.println("New Salary: " + newSalary[i]);
-            System.out.println();
-        }
-
-        System.out.println("Total Old Salary of all employees: " + totalOldSalary);
-        System.out.println("Total Bonus Payout: " + totalBonus);
-        System.out.println("Total New Salary of all employees: " + totalNewSalary);
+        System.out.println("Youngest friend is: " + names[youngestIndex] + " with age " + age[youngestIndex]);
+        System.out.println("Tallest friend is: " + names[tallestIndex] + " with height " + height[tallestIndex]);
 
         sc.close();
     }
